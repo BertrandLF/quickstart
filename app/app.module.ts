92495c882e13d1d8bba5cc1135ae853e199faf0b
Bertrand Le Foulgoc <bertrandlf@gmail.com>
2016-12-31 13:19:54 +0100

    Use in memory db over http for hero service
     - convention of angular-in-memory-web-api is api/{objectName}
     - returning muppets as well as heroes to show url convention

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
+import { AppRoutingModule } from './app.routing.module';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './details/hero-detail.component';
@@ -8,13 +15,13 @@ import { HeroesComponent }  from './list/heroes.component'
 import { HeroService } from './common/hero.service'
 import { DashboardComponent } from './dashboard/dashboard.component';
 
-import { AppRoutingModule } from './app.routing.module';
-
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService)
   ],
   declarations: [
     AppComponent,
---
+++ app/in-memory-data.service.ts
@@ -0,0 +1,23 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    let heroes = [
+      {id: 11, name: 'Mr. Nice'},
+      {id: 12, name: 'Narco'},
+      {id: 13, name: 'Bombasto'},
+      {id: 14, name: 'Celeritas'},
+      {id: 15, name: 'Magneta'},
+      {id: 16, name: 'RubberMan'},
+      {id: 17, name: 'Dynama'},
+      {id: 18, name: 'Dr IQ'},
+      {id: 19, name: 'Magma'},
+      {id: 20, name: 'Tornado'}
+    ];
+    let muppets = [
+      {id: 1, name: 'Kermit'},
+      {id: 2, name: 'Aidan'},
+      {id: 3, name: 'Ben'}
+    ]
+    return {heroes, muppets};
+  }
+}
